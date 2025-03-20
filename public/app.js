140d55cbbb341a73d389f6a0198dc28dc7d044ed
drewbyts <andrewrzs23@gmail.com>
2025-03-19 20:44:57 -0700

    reformatting entire website

--- public/app.js
+++ public/app.js
@@ -1,49 +1,38 @@
-const bars = document.querySelector('#bars');
-const menu = document.querySelector('#menu');
 
-bars.addEventListener('click', () => {
-    if (menu.classList.contains('hidden') && window.
-    innerWidth < 768) {
-        menu.classList.remove('hidden');
-        menu.classList.add(
-            'flex',
-            'flex-col',
-            'h-screen',
-            'text-6xl',
-            'uppercase',
-            'font-body',
-            'text-center',
-            'bg-gray-100',
-            'w-full',
-            'mt-4',
-            'absolute',
-            'top-8'
-        );
-    } else {
-        menu.classList.add('hidden');
-    }
-});
+// Define your project data
+const projects = [
+    {
+        title: "R: Markdown and Tidyverse",
+        link: "https://drewbyts.github.io/Market-Analysis/",
+        image: "https://images.unsplash.com/photo-1444653614773-995cb1ef9efa?auto=format&fit=crop&w=1776&q=80",
+    },
+    {
+        title: "SQL",
+        link: "https://dune.xyz/drewbyts/",
+        image: "./images/dune_analytics.png",
+    },
+];
 
-menu.addEventListener('click', () => {
-    menu.classList.add('hidden');
-})
+// Select the container where the project cards will be added
+const projectContainer = document.querySelector("#project-container");
 
-window.addEventListener('resize',() => {
-    if(window.innerWidth >768) {
-        menu.classList.add('hidden');
-        menu.classList.remove(
-            'flex',
-            'flex-col',
-            'h-screen',
-            'text-6xl',
-            'uppercase',
-            'font-body',
-            'text-center',
-            'bg-gray-100',
-            'w-full',
-            'mt-4',
-            'absolute',
-            'top-8'
-        );
-    }
+// Loop through the projects array and add cards dynamically
+projects.forEach((project) => {
+    projectContainer.innerHTML += `
+        <div class="p-4 lg:w-1/2">
+            <div class="h-full border-2 border-gray-800 border-opacity-60 rounded-lg flex flex-col">
+                <a href="${project.link}" target="_blank" rel="noopener noreferrer">
+                    <img
+                        class="w-full h-48 object-cover rounded-t-md"
+                        src="${project.image}"
+                        alt="${project.title}"
+                        loading="lazy"
+                    >
+                    <div class="bg-gray-100 font-body font-extralight p-4 rounded-b-md flex-grow">
+                        <h2 class="text-xs title-font font-body text-green-800 mb-1">${project.title}</h2>
+                    </div>
+                </a>
+            </div>
+        </div>
+    `;
 });
