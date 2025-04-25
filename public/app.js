59cb82b4300774fa97e9b1bce2819f9039f8b920
drewbyts <andrewrzs23@gmail.com>
2025-04-25 16:49:44 -0700

    Updated project layout and added case study card

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ const projects = [
         image: "https://images.unsplash.com/photo-1566577739112-5180d4bf9390?q=80&w=3126&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-        title: "Digital Scratchers - Sequential Testing*",
+        title: "Digital Scratchers - React*",
         link: "https://digital-lotto.netlify.app/",
         image: "https://plus.unsplash.com/premium_photo-1718191345906-39c178e43133?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
@@ -26,11 +26,11 @@ const projectContainer = document.querySelector("#project-container");
 // Loop through the projects array and add cards dynamically
 projects.forEach((project) => {
     projectContainer.innerHTML += `
-        <div class="p-4 lg:w-1/2">
-            <div class="h-full border-2 border-gray-800 border-opacity-60 rounded-lg flex flex-col">
+        <div class="p-4 lg:w-2/3">
+            <div class="max-h-full border-2 border-gray-800 border-opacity-60 rounded-lg hover:shadow-lg hover:scale-[2.00] transition-transform—">
                 <a href="${project.link}" target="_blank" rel="noopener noreferrer">
                     <img
-                        class="w-full h-48 object-cover rounded-t-md"
+                        class="w-full h-24 object-cover rounded-t-md"
                         src="${project.image}"
                         alt="${project.title}"
                         loading="lazy"
